9bd6e832575061e2c9d53dfeb16e045436583211
Zaur Suleimanov <rovasuzbeelze.fmb@gmail.com>
2021-09-15 14:41:09 +0200

    swift xc12 exploration unit 3 part 2

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        colorView.backgroundColor = .black
     }
 
 
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            colorView.backgroundColor = .red
+        } else {
+            colorView.backgroundColor = .black
+        }
+    }
 }
 
